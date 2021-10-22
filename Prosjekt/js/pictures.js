7157da55bec49945679d12993ec06783b168f19c
ineakr <ineak@stud.ntnu.no>
2021-10-22 12:45:15 +0200

    Fungerende omOss-side. Kun småting som gjenstår

--- Prosjekt/js/pictures.js
+++ Prosjekt/js/pictures.js
@@ -4,7 +4,7 @@
 //function to make the pop-window show
 function visible() {
     //let athlete = x //Hver atlet har en tekst hver
-    document.querySelector('.pop').style.display="flex";
+    let pop = document.querySelector('.pop').style.display="flex";
 }
 
 
@@ -14,52 +14,89 @@ function hidden() {
 }
 
 //close window
-let close = document.getElementById('close');
-close.addEventListener('click', (event) => {
+let closeButton = document.getElementById('close');
+closeButton.addEventListener('click', (event) => {
     hidden()
-    
 })
 
 
 // calling trainer
-let callTrainer = document.getElementById('trainer');
+let callTrainer = document.getElementsByClassName('trainer');
 let callTrainerText = document.getElementById('trainer_text');
-callTrainer.addEventListener('click', (event) => {
-    visible();
-    callTrainerText.style.display = "flex";
-})
+for (let i=0; i< callTrainer.length; i++) {
+    callTrainer[i].addEventListener('click', (event) => {
+        visible();
+        callTrainerText.style.display = "block";
+        //makes text disappear again
+    closeButton.addEventListener('click', (event)=> {
+        callTrainerText.style.display = "none";
+    })
+    }, false)
+}
+
+  
 
 
 
 //calling athlete 1
-let callAthlete1 = document.getElementById('athlete1');
+let callAthlete1 = document.getElementsByClassName('athlete1');
 let callAthlete1Text = document.getElementById('athlete1_text');
-callAthlete1.addEventListener('click', (event) => {
-    visible();
-    callAthlete1Text.style.display = "flex";
-})
+//event to both image and name
+for (let i=0; i< callAthlete1.length; i++) {
+    callAthlete1[i].addEventListener('click', (event) => {
+        visible();
+        callAthlete1Text.style.display = "block";
+
+    //makes text disappear again
+    closeButton.addEventListener('click', (event)=> {
+        callAthlete1Text.style.display = "none";
+    })
+    }, false)
+}
 
 //calling athlete 2
-let callAthlete2 = document.getElementById('athlete2');
+let callAthlete2 = document.getElementsByClassName('athlete2');
 let callAthlete2Text = document.getElementById('athlete2_text');
-callAthlete2.addEventListener('click', (event) => {
-    visible();
-    callAthlete2Text.style.display = "flex";
-})
+for (let i=0; i< callAthlete2.length; i++) {
+    callAthlete2[i].addEventListener('click', (event) => {
+        visible();
+        callAthlete2Text.style.display = "block";
+
+    //makes text disappear again
+    closeButton.addEventListener('click', (event)=> {
+        callAthlete2Text.style.display = "none";
+    })
+    }, false)
+}
 
 //calling athlete 3
-let callAthlete3 = document.getElementById('athlete3');
+let callAthlete3 = document.getElementsByClassName('athlete3');
 let callAthlete3Text = document.getElementById('athlete3_text');
-callAthlete3.addEventListener('click', (event) => {
-    visible();
-    callAthlete3Text.style.display = "flex";
-})
+for (let i=0; i< callAthlete3.length; i++) {
+    callAthlete3[i].addEventListener('click', (event) => {
+        visible();
+        callAthlete3Text.style.display = "block";
+
+    //makes text disappear again
+    closeButton.addEventListener('click', (event)=> {
+        callAthlete3Text.style.display = "none";
+    })
+    }, false)
+}
 
 //calling athlete 4
-let callAthlete4 = document.getElementById('athlete4');
+let callAthlete4 = document.getElementsByClassName('athlete4');
 let callAthlete4Text = document.getElementById('athlete4_text');
-callAthlete4.addEventListener('click', (event) => {
-    visible();
-    callAthlete4Text.style.display = "flex";
-})
+for (let i=0; i< callAthlete4.length; i++) {
+    callAthlete4[i].addEventListener('click', (event) => {
+        visible();
+        callAthlete4Text.style.display = "block";
+
+    //makes text disappear again
+    closeButton.addEventListener('click', (event)=> {
+        callAthlete4Text.style.display = "none";
+    })
+    }, false)
+}
+
 
